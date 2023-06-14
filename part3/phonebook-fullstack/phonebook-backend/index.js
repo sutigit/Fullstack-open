978a5805e78913cabbe919ec1463c2e95adaf958
sutigit <panyanilben@gmail.com>
2023-06-14 22:35:25 +0300

    frontend production build

--- part3/phonebook-fullstack/phonebook-backend/index.js
+++ part3/phonebook-fullstack/phonebook-backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require("morgan")
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors())
 
 let persons = [
     { 
@@ -78,7 +80,7 @@ app.post('/api/persons', (request, response) => {
     response.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
