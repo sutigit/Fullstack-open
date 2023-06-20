218940b84cd6a2b30c73c8f1e5acb74a91c44d85
sutigit <panyanilben@gmail.com>
2023-06-20 22:25:31 +0300

    part c done

--- part3/phonebook-fullstack/phonebook-backend/index.js
+++ part3/phonebook-fullstack/phonebook-backend/index.js
@@ -13,83 +13,94 @@ app.use(morgan('tiny'))
 app.use(cors())
 
 
-
-let persons = [
-    { 
-        "id": 1,
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-    },
-    { 
-        "id": 2,
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-    },
-    { 
-        "id": 3,
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-    },
-    { 
-        "id": 4,
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-    }
-]
-
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({}).then(persons => {
         response.json(persons)
     })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
+
 
 app.get('/info', (request, response) => {
     response.send("<p>Phonebook has info for " + persons.length + " people</p>" + new Date())
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+    // const id = Number(request.params.id)
+    // persons = persons.filter(person => person.id !== id)
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.json(result)
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 
-    response.status(204).end()
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     if (!request.body.name || !request.body.number) {
         return response.status(400).json({
             error: "content missing"
         })
     }
 
-    if (persons.some(person => person.name === request.body.name)) {
-        return response.status(400).json({
-            error: "name must be unique"
-        })
-    }
+    const body = request.body
     
-    const newPerson = {
-        id: Math.floor(Math.random() * 1000000),
-        name: request.body.name,
-        number: request.body.number
-    }
+    const person = new Person({
+      name: body.name,
+      number: body.number || null,
+    })
 
-    persons = persons.concat(newPerson)
-
-    response.json(newPerson)
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+        response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+
+  // this has to be the last loaded middleware.
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
