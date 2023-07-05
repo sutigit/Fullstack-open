e5edf015b48ae17ef638281065ae848022899461
sutigit <panyanilben@gmail.com>
2023-07-05 11:43:09 +0300

    change

--- part4/bloglist-fullstack/bloglist-backend/controllers/blogs.js
+++ part4/bloglist-fullstack/bloglist-backend/controllers/blogs.js
@@ -1,27 +1,40 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
+
     if (!body.title || !body.author) {
-        response.status(400).end()
+        response.status(400).send("missing title or author").end()
         return
     }
+
+
+    // It iis wad id iis for now
+    const userId = "64a51e7ce3df191c6028efb3"
+
+    const user = await User.findById(userId)
 
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes || 0
+        likes: body.likes || 0,
+        user: user.id
     })
 
     const savedBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog)
 })
 
